79762640404413de2ab37b3bacb735d90a44ea13
vijay <vijaygohel5454@gmail.com>
2022-02-18 15:59:27 +0530

    used mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyparser= require("body-parser");
 const date = require(__dirname+"/date");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,18 +10,32 @@ app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-var todoItems=["Buy Food","Wash cloths"];
-var workItems=[];
+mongoose.connect("mongodb://localhost:27017/TodoList");
+
+const todoSchema = {
+    name:String
+}
+
+const todo = mongoose.model("todoList",todoSchema);
+
+// var todoItems=["Buy Food","Wash cloths"];
+// var workItems=[];
+
+// const task1 = new todo({name:"Buy food"});
+
+// task1.save();
 
 app.get("/" , (req,res)=>{
     
-
-    res.render("todo", {Title:date.getDate(), todoItems:todoItems});
+    todo.find((err,result)=>{
+        res.render("todo", {Title:date.getDate(), todoItems:result});
+    })
+    
 })
 
-app.get("/work" , (req,res)=>{
-    res.render("todo", {Title:"Work List", todoItems:workItems});
-})
+// app.get("/work" , (req,res)=>{
+//     res.render("todo", {Title:"Work List", todoItems:workItems});
+// })
 
 app.post("/", (req,res)=>{
     
@@ -31,9 +46,26 @@ app.post("/", (req,res)=>{
     }
     else
     {
-        todoItems.push(req.body.newItem);
+        // todoItems.push(req.body.newItem);
+        new todo({
+            name:req.body.newItem
+        }).save();
         res.redirect("/");
     }
+})
+
+app.post("/delete",(req,res)=>{
+
+    console.log(req.body);
+
+    todo.deleteOne({_id:{$eq:req.body.checkbox}},(err)=>{
+        if(err)
+                console.log(err);
+            else
+                console.log("Item deleted successfully");
+    })
+    res.redirect("/");
+
 })
 
 app.listen(3000 , ()=>{
